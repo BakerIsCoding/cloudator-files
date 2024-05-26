4a4601660b01fecef7010784fc0284877be0c9de
EdNuGa <eduardo.nunez@ieselcalamot.com>
2024-05-26 17:34:55 +0200

    1.0.10
    Añadido endpoint para borrar carpeta de usuario cuando se borra la cuenta.
    Modificado application.properties para aumentar el tamaño de archivos a subir.
    Modificado application.properties para dejar los enlaces del servidor web.

--- src/main/java/com/cloudator/files/cloudatorfiles/controllers/DeleteFilesController.java
+++ src/main/java/com/cloudator/files/cloudatorfiles/controllers/DeleteFilesController.java
@@ -1,5 +1,13 @@
 package com.cloudator.files.cloudatorfiles.controllers;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +23,51 @@ public class DeleteFilesController {
 
     @PostMapping("/file/delete")
     public ResponseEntity<String> deleteFile(@RequestParam("id") Long id, @RequestParam("filename") String filename) {
-        //Se usa la clase java.io.File para manejar archivos del sistema.
+        // Se usa la clase java.io.File para manejar archivos del sistema.
         String filePath = directory + id + java.io.File.separator + filename;
 
         java.io.File file = new java.io.File(filePath);
 
         if (file.exists()) {
             if (file.delete()) {
-                return new ResponseEntity<>("File deleted successfully.", HttpStatus.OK);
+                return new ResponseEntity<>("Archivo borrado con éxito.", HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Failed to delete the file.", HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>("Error al borrar el archivo.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
-            return new ResponseEntity<>("File not found.", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Archivo no encontrado.", HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping("/file/account/delete")
+    public ResponseEntity<String> deleteAccount(@RequestParam("id") Long id) {
+
+        String directoryPath = directory + id;
+
+        Path pathToBeDeleted = Paths.get(directoryPath);
+
+        if (Files.exists(pathToBeDeleted)) {
+            try {
+                Files.walkFileTree(pathToBeDeleted, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                return new ResponseEntity<>("Cuenta borrada con éxito.", HttpStatus.OK);
+            } catch (IOException e) {
+                return new ResponseEntity<>("Error al borrar la cuenta.", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>("Cuenta no encontrada.", HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
